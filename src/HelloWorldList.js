2383576350512d447164080edb239a9ae82e194e
Ole Kristian Skramstad <okristian1@gmail.com>
2016-12-20 15:38:27 +0100

    react tutorial

---
+++ src/HelloWorldList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import HelloWorld from './HelloWorld';
+import AddGreeter from './AddGreeter';
+
+class HelloWorldList extends Component {
+    constructor(props) {
+        super(props);
+        this.addGreeting = this.addGreeting.bind(this);
+        this.state = { greetings: ['Jim,', 'Sally', 'Bender'] };
+    }
+    addGreeting(newName) {
+    this.setState({ greetings: [...this.state.greetings, newName] });
+    }
+    renderGreetings() {
+        return this.state.greetings.map(name => (
+            <HelloWorld key={name} name={name}/>
+        ));
+    }
+    render() {
+        return (
+            <div className="HelloWorldList">
+            <AddGreeter addGreeting={this.addGreeting}/>
+            {this.renderGreetings()}
+            </div>
+        );
+    }
+}
+
+export default HelloWorldList;
