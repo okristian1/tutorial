e8c0262532286753332ca695a2c363f339074348
Ole Kristian Skramstad <okristian1@gmail.com>
2016-12-20 23:43:19 +0100

    tutorial done

--- src/HelloWorldList.js
+++ src/HelloWorldList.js
@@ -6,14 +6,25 @@ class HelloWorldList extends Component {
     constructor(props) {
         super(props);
         this.addGreeting = this.addGreeting.bind(this);
+        this.removeGreeting = this.removeGreeting.bind(this);
         this.state = { greetings: ['Jim,', 'Sally', 'Bender'] };
     }
     addGreeting(newName) {
     this.setState({ greetings: [...this.state.greetings, newName] });
     }
+    removeGreeting(removeName) {
+        const filteredGreetings = this.state.greetings.filter(name => {
+            return name !== removeName;
+        });
+        this.setState({ greetings: filteredGreetings});
+    }
     renderGreetings() {
         return this.state.greetings.map(name => (
-            <HelloWorld key={name} name={name}/>
+            <HelloWorld
+             key={name} 
+             name={name}
+             removeGreeting={ this.removeGreeting }
+             />
         ));
     }
     render() {
